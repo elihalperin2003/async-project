aaa470722813729c0fb22688928bd1b50d603b81
eli <eelihalperin@gmail.com>
2026-07-01 15:31:13 +0300

    mission accomplished - 2

--- async-files.js
+++ async-files.js
@@ -2,9 +2,19 @@ const fs = require("fs");
 
 // 1
 
-fs.readFile("inpxhfdhut.txt", "utf-8", (err, data) => {
+// fs.readFile("input.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     return console.log(`Error while reading:\n ${err}`);
+//   }
+//   return console.log(`detail: ${data}`);
+// });
+
+// 2
+
+const outputText = "writed successfully by Node.js";
+fs.writeFile("output.txt", outputText, (err) => {
   if (err) {
-    return console.log(`Error in reading:\n ${err}`);
+    return console.log(`Error while reading:\n ${err}`);
   }
-  return console.log(`detail: ${data}`);
+  return console.log("File writed successfully");
 });
