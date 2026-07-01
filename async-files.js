b0d241cf3b11ad29dc68b89dd3192fabbe792c6c
eli <eelihalperin@gmail.com>
2026-07-01 16:17:11 +0300

    mission accomplished - questions. create .gitignore

--- async-files.js
+++ async-files.js
@@ -48,20 +48,31 @@ const fsPromises = require("fs").promises;
 // fsPromises
 //   .readFile("input.txt", "utf-8")
 //   .then((data) => {
-//     return fsPromises.writeFile("output-upper", data.toUpperCase());
+//     return fsPromises.writeFile("output-upper.txt", data.toUpperCase());
 //   })
 //   .then(() => console.log("Everything ending successfully"))
 //   .catch(console.error);
 
 // bonus
 
-Promise.all([
-  fsPromises.readFile("input.txt", "utf8"),
-  fsPromises.readFile("output.txt", "utf8"),
-])
-  .then((data) => {
-    return console.log(
-      `input.txt - ${data[0].length}, output.txt - ${data[1].length}`,
-    );
-  })
-  .catch(console.error);
+// Promise.all([
+//   fsPromises.readFile("input.txt", "utf8"),
+//   fsPromises.readFile("output.txt", "utf8"),
+// ])
+//   .then((data) => {
+//     return console.log(
+//       `input.txt - ${data[0].length}, output.txt - ${data[1].length}`,
+//     );
+//   })
+//   .catch(console.error);
+
+// questions
+
+// 1 :
+// fs - callbecks, fs.Promise - promises.
+// 2 :
+// בגלל שמדובר בפונקציה אסיכרונית הדרך היחידה לודא שהפונקציות יעבדו בסדר מסוים צריך לשים אותם אחת בתוך השנייה
+// 3 :
+// חוסר שרשור, catch אחד לכל הפונקציות
+// 4 :
+// הקוד לא יתקדם לשורה הבאה
